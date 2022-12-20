db4f724dea67e7c897ec526d5f2f244be58471be
nado1001 <tk.tech1001@gmail.com>
2022-12-21 01:29:57 +0900

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import image from '@astrojs/image';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import image from '@astrojs/image'
+import sitemap from '@astrojs/sitemap'
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })]
-});
+  site: 'https://astro-microcms.pages.dev',
+  integrations: [
+    react(),
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    sitemap(),
+  ],
+})
